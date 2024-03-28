96485d92ea77a998477a4df4a11975afd668d1eb
Akanit Kanoknak <akanit.kanoknak@gmail.com>
2024-03-28 13:50:41 +0700

    first commit

---
+++ app/Controllers/Masterboi.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Controllers;
+
+class Masterboi extends BaseController
+{
+    // public function index(): string
+    // {
+    //     return view('welcome_message');
+    // }
+
+
+
+    public function management(): string 
+    {
+        return view('mastermanagement');
+    }
+
+
+
+}
