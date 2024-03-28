4506351d6258cbb5b09a04265cc414e2a47d6559
Akanit Kanoknak <akanit.kanoknak@gmail.com>
2024-03-28 17:05:06 +0700

    update auth front

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -13,11 +13,13 @@ $routes->get('/test', 'Home::Test');
 $routes->group('admin',static function($routes){
 
     $routes->group('',[],static function($routes){
-        $routes->view('example-page','example-page');
+        // $routes->view('example-page','example-page');
+        $routes->get('home','AdminController::index',['as'=>'admin.home']);
     });
 
     $routes->group('',[],static function($routes){
-        $routes->view('example-auth','example-auth');
+        // $routes->view('example-auth','example-auth');
+        $routes->get('login','AuthController::loginForm',['as'=>'admin.login.form']);
     });
 
 });
--- app/Views/example-auth.php
+++ app/Views/example-auth.php
@@ -1,1 +1,6 @@
-example auth....
+<?php $this->extend('backend/layout/auth-layout') ?>
+<?php $this->section('content')  ?>
+
+---- Auth pages content here ----
+
+<?php $this->endSection(); ?>
